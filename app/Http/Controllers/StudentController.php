8b99111b3228556ed09c890627947bb62a2d8efa
M40 <modev.404@gmail.com>
2024-12-17 08:43:36 +0300

    getStudents

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     //
+    public function index()
+    {
+        $students = Student::all();
+        return json_encode($students);
+    }
 }
