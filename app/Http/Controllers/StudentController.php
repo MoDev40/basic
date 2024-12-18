0f3ee185592709c39fb7cc2cbff0dcbab3068e23
M40 <modev.404@gmail.com>
2024-12-18 19:02:23 +0300

    controller resources

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,17 +7,26 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $students = Student::all();
         return view('components.students', ['students' => $students]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('components.create');
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $req)
     {
         $data = $req->validate([
@@ -29,5 +38,37 @@ class StudentController extends Controller
         ]);
         Student::create($data);
         return redirect('students')->with('success', 'Student has been created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Student $student)
+    {
+        //
     }
 }
